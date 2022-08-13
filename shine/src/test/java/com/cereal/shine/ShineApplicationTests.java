7fa0ade7779d1dfa939565f13438c27ca658a203
Srijan <mail.srizan@gmail.com>
2022-08-13 14:03:04 -0700

    new project files

---
+++ shine/src/test/java/com/cereal/shine/ShineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cereal.shine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
